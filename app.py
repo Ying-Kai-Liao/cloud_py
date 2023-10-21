1fc27a7d3ce6ae24aa6a17f425ae5308aa5fb1bf
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-10-21 17:26:57 +0800

    till 1020

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-# Data of Posts
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is the content of post 1. BlaBlaBla......',
-        'author' : 'Rebecca'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the content of post 2. BlaBlaBla......'
-    }
-]
 
 app = Flask(__name__)
 
@@ -24,7 +11,41 @@ def index():
 
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template("posts.html", posts = all_posts)
+    
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_author = request.form['author']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template("posts.html", posts = all_posts)
+    
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    
+    post = BlogPost.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
 
 @app.route('/test')
 def hello():
@@ -41,10 +62,6 @@ def about():
 @app.route('/readme')
 def readme():
     return "This is a read me page. Please read me first!!"
-
-@app.route('/readme')
-def readme2():
-    return "hehe, broken"
 
 @app.route('/home/posts/<int:id>')
 def hello2(id):
