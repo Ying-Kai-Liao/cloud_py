70cdad4a270aaec1d77fbf347660503ebaa51d34
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-10-21 13:59:05 +0800

    index

--- api/index.py
+++ api/index.py
@@ -1,9 +1,15 @@
+
 from flask import Flask
+from flask import render_template
 
 print(__name__)
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/test')
 def hello():
     return 'Hello, World!'
